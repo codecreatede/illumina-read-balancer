78b1cc5d63030586da501d597be4ed1491631546
Gaurav Sablok <codeprog@icloud.com>
2024-08-27 11:07:42 +0200

    codecreatede
    modt og the bug fixed, added sample files and adding the support for the HTTP route and network routing.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # illumina read-balancer
 
 - a go faster implementation of illumina read balancer for novaseq, nextseq reads. 
-- A C version is here : https://github.com/linsalrob/fastq-pair. The go version is much shorter, can be implemented in concurrency and also as a http browser module
+- A C version is here : https://github.com/linsalrob/fastq-pair. The go version is much shorter, can be implemented in concurrency and also as a http browser.
 
 ```
 go run main.go
--- main.go
+++ main.go
@@ -3,51 +3,48 @@ package fastq
 /*
 
 Author Gaurav Sablok
-Universitat Potsdam 
+Universitat Potsdam
 Date : 2024-8-27
 
 a multiplex nextseq, novaseq read balancer for the Illumina reads using GO.
+adding the support for the MongoDB file creation and a concurrent pattern.
 
 */
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
+
 	argsinput1 := os.Args[1:]
 	argsinput2 := os.Args[2:]
-	argsoutput1 := os.Args[3:]
-	argsoutput2 := os.Args[4:]
-	if argsinput1 || argsinput2 || argsinput3 || argsinput4 == nil {
+	readinput1, err := os.OpenFile(argsinput1)
+	if err != nil {
+		panic(errors.New())
+		log.Fatal(errors.New("input1 cant be empty"))
+	}
+	readinput2, err := os.OpenFile(argsinput2)
+	if err != nil {
 		panic(err)
-		log.Fatal(err.Error.new("The arguments cant be empty"))
+		log.Fatal(Error.New("input2 cant be empty"))
 	}
-    
-    readinput1, err := os.OpenFile(argsinput1)
-    if err != nil {
-    		panic(err.Error())
-    		log.Fatal(err)
-    	}
-    readinput2 := os.OpenFile(argsinput2)
-    if err != nil {
-    		panic(err.Error())
-    		log.Fatal(err)
-    	}
-    open1 := bufio.NewScanner(readinput1)
-    open2 := bufio.NewScanner(readinput2)
+	open1 := bufio.NewScanner(readinput1)
+	open2 := bufio.NewScanner(readinput2)
 
-    var openinputH1 []string
-    var openinputSeq1 []string
-    var openinputH2 []string
-    var openinputSeq2 []string
+	openinputH1 := []string{}
+	openinputSeq1 := []string{}
+	openinputH2 := []string{}
+	openinputSeq2 := []string{}
 
-    for open1.Scan() {
+	for open1.Scan() {
 		line := open1.Text()
-		if string(line).HasPrefix("@") || strings.Contains(string(line), "length") {
+		if strings.HasPrefix(line, "@") || strings.Contains(string(line), "length") {
 			openinputH1 = append(openinputH1, strings.Split(line, " ")[0])
 		}
 		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" || string(line[0]) == "C" {
@@ -55,37 +52,37 @@ func main() {
 		}
 	}
 
-	 for open2.Scan() {
+	for open2.Scan() {
 		line := open2.Text()
-		if string(line).HasPrefix("@") || strings.Contains(string(line), "length") {
-			openinputH1 = append(openinputH1, strings.Split(line, " ")[0])
+		if strings.HasPrefix(line, "@") || strings.Contains(string(line), "length") {
+			openinputH1 = append(openinputH2, strings.Split(line, " ")[0])
 		}
 		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" || string(line[0]) == "C" {
-			openinputSeq1 = append(openinputSeq1, line)
+			openinputSeq2 = append(openinputSeq2, line)
 		}
 	}
-    
-    balancedH1 = []string
-    balancedH2 = []string
-    balancedH1head = []string
-    balancedH2head = []string
 
-    for i := range openinputH1 {
-        if string(openinputH1[i]) == string(openinput[H2]) {
-        	balancedH1 = append(balancedH1, openinputH1[i])
-        	balancedH2 = append(balancedH2, openinputH2[i])
-        	balancedH1head = append(balancedH1head = openinputSeq1[i])
-        	balancedH2head = append(balancedH2head = openinputSeq[i])
-        }
-    }
+	balancedH1 := []string{}
+	balancedH2 := []string{}
+	balancedH1head := []string{}
+	balancedH2head := []string{}
 
-    len, err := len(balancedH1) == len(balancedH1) 
-    if err != nil {
-    	panic(err.Error())
-    	log.Fatal(Error.new("The balancer is uneven and the reads cant be assembled"))
+	for i := range openinputH1 {
+		if string(openinputH1[i]) == string(openinputH2[i]) {
+			balancedH1 = append(balancedH1, openinputH1[i])
+			balancedH2 = append(balancedH2, openinputH2[i])
+			balancedH1head = append(balancedH1head, openinputSeq1[i])
+			balancedH2head = append(balancedH2head, openinputSeq2[i])
+		}
+	}
 
-    } else {
-    	fmt.println("The balancer is equal")
-    }
+	checklen := len(balancedH1) == len(balancedH1)
+	if checklen != true {
+		panic(err.Error())
+		log.Fatal(Error.new("The balancer is uneven and the reads cant be assembled"))
+
+	} else {
+		fmt.Println("The balancer is equal")
+	}
 
 }
